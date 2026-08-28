d90d6e28bd73523c63e309581d5f7d914d30222c
Dipeshchand <dipeshchand972@gmail.com>
2026-08-28 12:55:07 +0530

    devconnect-kubernetes

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { name, age } = req.body;
+
+    const user = await User.create({
+      name,
+      age,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("DevConnect backend is running 🚀");
+});
+
+app.use("/users",userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
